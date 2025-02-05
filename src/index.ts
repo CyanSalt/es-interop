4ab2a6b879b05f3b7eeab67a600dbbd83ac658a6
CyanSalt <c.salt@hotmail.com>
2025-02-05 17:27:43 +0800

    feat: add resolveCJSModule

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,17 @@ function getImporterContext(importer: string) {
   return path.dirname(filename)
 }
 
-export async function resolveModule(id: string, importer: string = getDefaultImporter()) {
+export function resolveCJSModule(id: string, importer: string = getDefaultImporter()): string {
+  const require = createRequire(importer)
+  return require.resolve(id)
+}
+
+export function loadCJSModule<T = any>(id: string, importer: string = getDefaultImporter()): T {
+  const require = createRequire(importer)
+  return require(id)
+}
+
+export async function resolveModule(id: string, importer: string = getDefaultImporter()): Promise<string> {
   if (URL.canParse(id) || path.isAbsolute(id)) return id
   const factory = new ResolverFactory({
     extensions: ['.js', '.mjs', '.cjs', '.json'],
@@ -31,11 +41,6 @@ export async function loadESModule<T = any>(id: string, importer: string = getDe
   return import(absolutePath)
 }
 
-export function loadCJSModule<T = any>(id: string, importer: string = getDefaultImporter()): T {
-  const require = createRequire(importer)
-  return require(id)
-}
-
 export async function loadTSModule<T = any>(id: string, importer: string = getDefaultImporter()): Promise<T> {
   return tsImport(id, importer)
 }
@@ -49,7 +54,8 @@ export function loadModule<T = any>(id: string, importer: string = getDefaultImp
   return loadESModule<T>(id, importer)
 }
 
-export function interopDefault<T>(mod: unknown): T {
+export function interopDefault<T = any>(mod: unknown): T {
   if (typeof mod !== 'object' || mod === null) return mod as T
-  return ('default' in mod ? mod.default : mod) as T
+  // e.g. `export default ...` --(tsc)-> `exports.default` --(dynamic import)-> `Module { default: { default: ... } }`
+  return 'default' in mod ? interopDefault<T>(mod.default) : mod as T
 }
